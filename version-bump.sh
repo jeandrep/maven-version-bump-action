b9f5f5f46f41ffc744530e950b73d9cb62fb1f1e
jeandrep <4304398+jeandrep@users.noreply.github.com>
2022-08-25 09:42:23 +0200

    Update version-bump.sh

--- version-bump.sh
+++ version-bump.sh
@@ -37,11 +37,11 @@ git config --global user.name $NAME
 OLD_VERSION=$($DIR/get-version.sh)
 
 BUMP_MODE="none"
-if git log -1 | grep -q "#major"; then
+if git log -1 | grep -q -i -E "major|breaking"; then
   BUMP_MODE="major"
-elif git log -1 | grep -q "#minor"; then
+elif git log -1 | grep -q -i -E "minor|feat"; then
   BUMP_MODE="minor"
-elif git log -1 | grep -q "#patch"; then
+elif git log -1 | grep -q -i -E "patch|fix|bug"; then
   BUMP_MODE="patch"
 fi
 
